c0a8db2f6c62c05f7f8e835a792c3662c09544b0
Batuhan Erdem <79407999+batuhanerdem@users.noreply.github.com>
2024-08-14 16:34:20 +0300

    gradle changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,9 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
         gradlePluginPortal()
     }
 }
